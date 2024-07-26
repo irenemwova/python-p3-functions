61cba6919398917d5dba354c784e82e87d841f32
Irene Mwova <irene.mwova@student.moringaschool.com>
2024-07-26 23:42:51 +0300

    complete the assessment

--- lib/testing/lib_test.py
+++ lib/testing/lib_test.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
-from functions import greet_programmer, greet, greet_with_default, \
+from .functions import greet_programmer, greet, greet_with_default, \
                         add, halve
 
 import io
